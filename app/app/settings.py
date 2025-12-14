f6cea8fbec8e4c0b47a40c9d344a6d270f02097c
BLAZER2283 <aramaiszur@gmail.com>
2025-12-14 16:15:20 +0300

    Update settings.py

--- app/app/settings.py
+++ app/app/settings.py
@@ -104,11 +104,14 @@ CORS_ALLOW_METHODS = [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dic',
+        'USER': 'asa',
+        'PASSWORD': '23449365Afg',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 
 RESULTS_ROOT = BASE_DIR / 'results'
